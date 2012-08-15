d3cab8e7d925f6f7c20390a1e1da09c15181e565
Istvan <iszukacs@riotgames.com>
2012-08-15 16:45:48 -0700

    Fux

--- src/cljmx/core.clj
+++ src/cljmx/core.clj
@@ -5,15 +5,19 @@
 (:gen-class))
 
 (defn -main [& args]
-  ;(println "Welcome to my project! These are your args:" args))
-  ;(println (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader))))
-  ;(doseq [i (jmx/mbean-names "*:*")] (prn i))
-  ;(doseq [j (jmx/mbean "java.lang:type=Memory")] (prn j))
+
   ;{:Verbose false, :ObjectPendingFinalizationCount 0, 
     ;:HeapMemoryUsage {:committed 85000192, :init 0, :max 129957888, :used 17811912}, 
     ;:NonHeapMemoryUsage {:committed 24317952, :init 24317952, :max 136314880, :used 16712040}} 
-  ;(println (jmx/mbean "java.lang:type=Memory"))
 
-  (println (get (get (jmx/mbean "java.lang:type=Memory") :HeapMemoryUsage) :committed))
+
+  (println (get-in (jmx/mbean "java.lang:type=Memory") [:HeapMemoryUsage :committed]))
+
+  (println (:b (:a {:a {:b 42}})))
+  (get-in {:a {:b 42}} [:a :b])
+
+  (def memory (jmx/mbean "java.lang:type=Memory"))
+  (get-in memory [:HeapMemoryUsage :committed])
+
 
 )

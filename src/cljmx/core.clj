30b283b629c39813de488784f114049707c22031
Istvan <iszukacs@riotgames.com>
2012-08-20 15:11:13 -0700

    1.0.1

--- src/cljmx/core.clj
+++ src/cljmx/core.clj
@@ -1,36 +1,28 @@
 (ns cljmx.core
   (:require [clojure.java.jmx   :as jmx], 
             [clojure.tools.cli  :as cli]
-  (:gen-class))
-)
+  (:gen-class)))
 
 (defn invoke [{:keys [host port bean]}]
-
   (println (str "Invoking on bean " bean " at " host ":" port))
-
   (defn alltehbeans [path]
     (map (memfn getCanonicalName) (jmx/mbean-names path)))
-
   (defn gimmethat [x]
     (doseq [s x] (println (jmx/mbean s)) ))
-
   (jmx/with-connection {:host host, :port port}
-    (gimmethat (alltehbeans bean)))
-)
+    (gimmethat (alltehbeans bean))))
 
 (defn run-with [[options args banner]]
   (when (:help options)
     (println banner)
     (System/exit 1))
-  (invoke options)
-)
+  (invoke options))
 
 (defn -main [& args]  
   (run-with (cli/cli args    
     ["-h" "--host" "Host to connect to" :default "localhost"]
     ["-p" "--port" "Port to connect to" :parse-fn #(Integer. %)]
     ["-b" "--bean" "Bean name"]
-    ["--help" "Show help" :default false :flag true]))
-)
+    ["--help" "Show help" :default false :flag true])))
 
 
